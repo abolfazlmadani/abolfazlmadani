355227119f4b82ec9f0477d800dbdbb7c09bed0e
Abolfazl05 <155589119+abolfazlmadani@users.noreply.github.com>
2025-01-17 16:15:15 +0330

    a

---
+++ Employees/Main.java
@@ -0,0 +1,67 @@
+package Employees;
+
+import java.sql.*;
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        var person = new Person();
+        Scanner scanner = new Scanner(System.in);
+
+        // نام کاربری و رکز عبور کارمند
+        System.out.print("user name :");
+        person.setUsername(scanner.nextLine().toUpperCase());
+        System.out.print("password :");
+        String password = scanner.next();
+
+        //رمز رندوم ریخته میشه تو متغیر برای نمایش
+        int correctPassword = person.random_password;
+        System.out.print("[password] :" + correctPassword+"\n");//نمایش رمز رندوم
+        System.out.print( "new Password :");
+        person.setPassword(scanner.nextInt());
+
+        // اطلاعات پایگاه داده
+        String url = "jdbc:mysql://localhost:3306/?user=root";
+        String dbUsername = "root";
+        String dbPassword = "abolfazl05";
+
+        try {
+            // اتصال به پایگاه داده
+            Connection connection = DriverManager.getConnection(url, dbUsername, dbPassword);
+            Statement statement = connection.createStatement();
+            statement.execute("use data_employees;");
+
+            // بررسی مطابقت داشتن کاربر با رمز عبور
+            String sql = "SELECT password FROM employees WHERE name = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, person.getUsername());
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            // مقایسه رمز عبور ورودی با رمز عبور ذخیره شده
+            if (resultSet.next())
+            {
+                String storedPassword = resultSet.getString("password");
+
+                if (storedPassword.equals(password)) {
+                    System.out.println("رمز عبور صحیح است.");
+                } else {
+                    System.out.println("رمز عبور نادرست است.");
+                }
+            } else {
+                System.out.println("کاربر یافت نشد.");
+            }
+            // بستن منابع
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+        }
+        catch (Exception e)
+        {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ Employees/Person.java
@@ -0,0 +1,69 @@
+package Employees;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Person {
+    Scanner scanner = new Scanner(System.in);//آبجکت ورودی
+
+    private String username;
+
+    Random random =new Random();// آبجکت رندوم
+
+    // اعداد تصادفی
+    public int random_password = random.nextInt(10000);
+    //تعداد تلاش password
+    public int passwords;  int correctPassword2;   int number = 3;
+    //متغیری برای برس صحت رمز عبور
+    public boolean Boolean1 = false;
+
+
+
+    public void setUsername(String username1)
+    {
+        this.username = username1;
+    }
+    public String getUsername()
+    {
+        return username;
+    }
+
+
+    public void setPassword(int password1) //اعتبار سنجی رمز ورود
+    {
+        if (password1 == random_password)
+        {
+            Boolean1 = true;
+            System.out.println("nice");
+        }
+        else
+        {
+            System.out.println("not password");
+
+            while (!Boolean1 && number>0 )
+            {
+                if (password1 != random_password)
+                {
+                    correctPassword2 = random.nextInt(10000);
+                    System.out.print("[pass] :" + correctPassword2 + "\n");
+                    System.out.print("new password :");
+                    passwords = scanner.nextInt();
+                    if (passwords==correctPassword2)
+                    {
+                        System.out.println("nice jab");
+                        break;
+                    }
+                    else
+                    {
+                        System.out.println("no pass");
+                        number--;
+                    }
+                    if (number==0)
+                    {
+                        throw new IllegalArgumentException("(((limit)))");
+                    }
+                }
+            }
+        }
+    }
+}
